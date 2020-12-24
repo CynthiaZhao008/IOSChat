18e42994ca98313619effa435372bdb9bfca6f2b
Cynthia <czhao9@pride.hofstra.edu>
2020-12-24 12:23:09 -0500

    Initial Commit

---
+++ IOSChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSChat
+//
+//  Created by Cynthia Zhao on 2020-12-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
